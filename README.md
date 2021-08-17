efef5f5f577741f0a5eee26354dbe0ee16079aa6
feiyongjing <2059326740@qq.com>
2021-08-17 22:20:08 +0800

    完成自动生成Dao层和entity类

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # online-mall
 电商和支付双系统
+
+mysql docker服务启动
+docker run --name mall -e MYSQL_DATABASE=mall -e MYSQL_ROOT_PASSWORD=root -p 3306:3306 -d mysql:8.0.19
---
+++ src/main/java/com/github/eric/mall/generate/mapper/UserMapper.java
@@ -0,0 +1,32 @@
+package com.github.eric.mall.generate.mapper;
+
+import com.github.eric.mall.generate.entity.User;
+import com.github.eric.mall.generate.entity.UserExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+@Mapper
+public interface UserMapper {
+    long countByExample(UserExample example);
+
+    int deleteByExample(UserExample example);
+
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    List<User> selectByExample(UserExample example);
+
+    User selectByPrimaryKey(Integer id);
+
+    int updateByExampleSelective(@Param("record") User record, @Param("example") UserExample example);
+
+    int updateByExample(@Param("record") User record, @Param("example") UserExample example);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKey(User record);
+}
